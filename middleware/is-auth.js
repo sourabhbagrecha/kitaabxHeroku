379d9aa47e4f37a0e590c755f935dd36e87bfb79
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2019-02-19 02:16:09 +0530

    what is kitaabx section added

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
   if(!req.session.isLoggedIn){
+    req.flash('error', 'You must login first!')
     return res.redirect('/auth/login');
   }
   next();
--- routes/publisher.js
+++ routes/publisher.js
@@ -7,6 +7,7 @@ const isPublisher = require('../middleware/is-publisher');
 const checkO = require('../middleware/check-ownership');
 const pController = require('../controllers/publisher');
 const awsController = require('../trash/aws-upload');
+const isAuth = require('../middleware/is-auth');
 
 router.get('/dashboard', isPublisher, pController.getDashboard);
 router.get('/createMaterial', isPublisher, pController.getCreateMaterialPage);
@@ -15,7 +16,7 @@ router.post('/createMaterial/step2', isPublisher, pController.postStep2);
 router.get('/createMaterial/:id/uploadpdf', isPublisher, checkO, pController.getStep2);
 // router.post('/createMaterial/:id/uploadpdf',pController.postPdfUpload);
 router.post('/createMaterial/:id/uploadpdf', multipartyMiddleware, awsController.multiparting);
-router.get('/signup', pController.getSignup);
-router.post('/signup', pController.postSignup);
+router.get('/signup', isAuth, pController.getSignup);
+router.post('/signup', isAuth, pController.postSignup);
 
 module.exports = router;
